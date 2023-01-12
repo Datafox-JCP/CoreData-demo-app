350abd3fd6d8b0910780f0c32fa07060cc7c3f27
Datafox-JCP <datafox@outlook.com>
2023-01-12 10:40:14 -0600

    Initial Commit

---
+++ CoreDataContacts/CoreDataContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataContactsApp.swift
+//  CoreDataContacts
+//
+//  Created by Juan Hernandez Pazos on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
